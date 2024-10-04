d1bc4035ea987a625a901f6e2bc7ca45b48fb3f5
Zbuddy13 <jzshaw13@gmail.com>
2024-10-04 10:36:27 -0400

    added comments

--- modules/github.py
+++ modules/github.py
@@ -3,6 +3,7 @@ from git import Repo
 import modules.config as config
 import modules.time as t
 
+# Used to push results to github
 def gitCommit(location, token):
     repo = Repo(location)
     os.chdir(location)
--- modules/timer.py
+++ modules/timer.py
@@ -1,11 +1,14 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+# Used to inialize the python timer
 def initializeTimer():
     scheduler = BlockingScheduler()
     return scheduler
 
+# Add a program to the timer
 def addJob(timer, program, time):
     timer.add_job(program,'interval', hours=int(time))
 
+# Start the timer
 def startTimer(scheduler):
     scheduler.start()
